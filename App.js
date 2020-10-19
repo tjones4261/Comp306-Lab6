747ab55407d5922d554903b927055be846edab42
John Barr <barr@ithaca.edu>
2020-10-19 14:35:21 -0400

    Update App.js
    Added comment

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// use this file to begin lab 6
 export default function App() {
   return (
     <View style={styles.container}>
